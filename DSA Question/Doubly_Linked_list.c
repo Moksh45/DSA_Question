a444f47a456552b74f89b1a96ce60a7dd477f352
Moksh Gupta <mokshgupta567@gmail.com>
2022-06-23 16:36:23 +0530

    add doubly linked list

--- DSA Question/Doubly_Linked_list.c
+++ DSA Question/Doubly_Linked_list.c
@@ -4,31 +4,37 @@ struct Node
 {
     int data;
     struct Node *next;
+    struct Node *previous;
 };
 
 void linkedListTraversal(struct Node *head)
 {
-    struct Node *ptr = head;
-    do
+    struct Node *ptr=head;
+    struct Node *p;
+    printf("\nTraversal in forward direction \n"); 
+    while (ptr!=NULL)
     {
-        printf("Element : %d\n", ptr->data);
-        ptr = ptr->next;
-    } while (ptr != head);
+        printf("Element : %d\n",ptr->data);
+        p=ptr;
+        ptr=ptr->next;
+    }
+   
+    printf("\nTraversal in reverse direction \n");
+    while (p!=NULL)
+    {
+        printf("Element : %d\n",p->data);
+        p=p->previous;
+    }
 }
 // case 1
 struct Node *insertAtFirst(struct Node *head, int data)
 {
     struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
     ptr->data = data;
-
-    struct Node *p = head->next;
-    while (p->next != head)
-    {
-        p = p->next;
-    }
-    p->next = ptr;
-    ptr->next = head;
-    head = ptr;
+    ptr->next=head;
+    ptr->previous=NULL;
+    head->previous=ptr;
+    
 
     return ptr;
 }
@@ -46,6 +52,7 @@ struct Node *insertAtIndex(struct Node *head, int data, int index)
     }
     ptr->data = data;
     ptr->next = p->next;
+    ptr->previous=p;
     p->next = ptr;
 
     return head;
@@ -95,31 +102,37 @@ int main()
 
     head->data = 12;
     head->next = second;
+    head->previous=NULL;
 
     // Link second and third nodes
     second->data = 123;
     second->next = third;
+    second->previous=head;
 
     // Link third and fourth nodes
     third->data = 532;
     third->next = fourth;
+    third->previous=second;
 
     // Link fourth and Fivth nodes
     fourth->data = 34;
     fourth->next = fivth;
+    fourth->previous=third;
 
     // Terminate the list at the fivth node
     fivth->data = 59;
     fivth->next = sixth;
+    fivth->previous=fourth;
 
     sixth->data = 1275;
-    sixth->next = head;
+    sixth->next = NULL;
+    sixth->previous=fivth;
 
     printf("Linked list before insertion\n");
     linkedListTraversal(head);
-    // head = insertAtFirst(head, 100);
-    // head = insertAtIndex(head, 100, 1);
-    head = insertAtEnd(head, 100);
+    //head = insertAtFirst(head, 100);
+    head = insertAtIndex(head, 100, 1);
+    //head = insertAtEnd(head, 100);
     // head = insertAfterNode(head, third, 100);
     printf("\nLinked list after insertion\n");
     linkedListTraversal(head);
